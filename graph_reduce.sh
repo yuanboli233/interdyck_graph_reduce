9d4adfcf9228ec3f1207127d5aa374ca38da527c
Yuanbo Li <yuanboli233@gmail.com>
2020-04-04 15:55:15 -0400

    make it executable

--- graph_reduce.sh
+++ graph_reduce.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
 cp current.dot orig.dot
 python3 graphgen_forfilter.py current.dot
 ./graph_simp.py
 python3 recover_single_direction.py orig.dot current.dot > result.dot
+mv result.dot current.dot
+rm orig.dot
+rm tmp.dot
